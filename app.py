0786b2d20b85b50fccfcc16e9384c5ae5d8bb66b
Sparsh Patil <dcpsparshpatil@gmail.com>
2026-04-27 11:32:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -510,6 +510,6 @@ def download_file(file_id):
     return send_from_directory(folder_path, file.filename, as_attachment=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     
